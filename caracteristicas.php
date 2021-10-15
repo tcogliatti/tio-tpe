763f1d61bf934d930a41ee5aea2d01fa0e66d67e
Jeremias Storm <jerestorm@gmail.com>
2021-10-15 11:20:53 -0300

    renombre de archivos html a php

---
+++ caracteristicas.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Características</title>
+</head>
+
+<body class="cuerpo">
+    <div class="container w-75 mt-3">
+        <img class="w-100" src="./images/header.png" alt="encabezado">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light mt-3">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-around" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="./index.html">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="estructura.html">Estructura</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="clasificacion.hmtl">Clasificación</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="caracteristicas.html">Características</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">Cultura</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">Equipo</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div>
+            <div class="container" id="menu">
+                <div>
+                    <ul class="navbar-nav">
+                        <li><a href="caracteristicas/objetivos.html" class="btn btn-info mt-5">Objetivos</a></li>
+                        <li><a href="caracteristicas/metas.html" class="btn btn-info mt-1">Metas</a></li>
+                        <li><a href="caracteristicas/recursosMateriales.html" class="btn btn-info mt-1">Recursos
+                                Materiales</a></li>
+                        <li><a href="caracteristicas/recursoshumanos.html" class="btn btn-info mt-1">Recursos
+                                Humanos</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <img src="images/vacunacionn.jpeg" alt="" class="img-caracteristicas">
+                </div>
+            </div>
+
+
+            <footer class="p-3 mb-2 bg-info text-white mt-5">
+                <p class="text-center mb-0">Trabajo Práctico Especial - Tecnologías de la información en las
+                    Organizaciones - Grupo 5</p>
+            </footer>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
+                crossorigin="anonymous"></script>
+
+</body>
+
+</html>
