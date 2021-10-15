cded312688d80d8cfafc61d1d6b5746cadb9a73c
Yazmin Villalba <yazminvillalba55@gmail.com>
2021-10-15 17:36:43 -0300

    agregue localizacion

--- clasificacion.php
+++ clasificacion.php
@@ -3,15 +3,14 @@
 <div>
     <div>
         <ul>
-            <h4 class="mt-3">Localizacion:</h4>
-            <li>
-
-            </li>
-            <h4 class="mt-3">Formalidad:</h4>
+            <h4 class="mt-3">Localizacion</h4>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3191.309948707484!2d-60.34119838417945!3d-36.882932789126365!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959445da011f336b%3A0xd3a952dd753395a4!2sTerritorial%202!5e0!3m2!1ses-419!2sar!4v1634329083515!5m2!1ses-419!2sar" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+         
+            <h4 class="mt-3">Formalidad</h4>
             <li>Organización formal, perteneciente al sector público,
                 sin fines de lucro y de resolución o servicios
             </li>
-            <h4 class="mt-3">Grado de centralización:</h4>
+            <h4 class="mt-3">Grado de centralización</h4>
             <li>Su grado de centralización en cuanto a la distribución de la prestación
                 del servicio, podemos considerarlo descentralizado, ya que el servicio
                 se distribuye de forma territorial en toda la provincia de Buenos Aires.
